af76f5b288c8a8d1c81941d7adfe47f0fe8c378a
fernandoscjunior <primeirasdezpesquisas@gmail.com>
2025-07-18 00:14:36 -0300

    adding home page

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -10,3 +10,16 @@ navButton.addEventListener('click', () => {
 //getting navigation menu
 const navBar = document.querySelector('#nav-bar');
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("#list-nav a");
+  // Obtém o nome da página atual (sem extensão)
+  const currentPage = window.location.pathname.split("/").pop().split(".")[0];
+    
+  links.forEach(link => {
+    const page = link.getAttribute("data-page");
+    if (page === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
